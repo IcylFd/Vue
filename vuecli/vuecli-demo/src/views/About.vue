c59710281a582f44dd3a0206ec54109e6b32f3c2
lifangdi <772167928@qq.com>
2018-08-09 16:49:49 +0800

    2018.8.9 vuecli

--- vuecli/vuecli-demo/src/views/About.vue
+++ vuecli/vuecli-demo/src/views/About.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+      <div>
+          <h2>局部方法</h2>
+        <button @click="add">+1</button>
+        <button @click="reduce">-1</button>
+        <div>{{num}}</div>
+      </div>
+      <div>
+          <h2>全局方法</h2>
+          <button @click="$store.commit('add',3)">+1</button>
+          <button @click="reduce">-1</button>
+          <button @click="actionAdd">action</button>
+          <div>{{count}}</div>
+      </div>
   </div>
 </template>
 
 <script>
+    import  {mapState,mapMutations,mapGetters,mapActions} from 'vuex';      //方法2
+
     export default {
 //        beforeRouteEnter(to, from, next) {
 ////            console.log(to);
@@ -15,5 +30,41 @@
 ////            console.log(to);
 ////            console.log(from);
 //        }
+
+
+        data(){
+            return{
+                num:0
+            }
+        },
+//        methods:{
+//            add(){
+//                this.num++;
+//            },
+//            reduce(){
+//                this.num--;
+//            }
+//        },
+//        methods:mapMutations(["add","reduce"]),
+        methods:{
+            ...mapMutations(["add","reduce"]),
+            ...mapActions(["actionAdd"])
+        },
+        computed:{
+            ...mapState(["count"]),
+            ...mapGetters(["count"])
+        }
+//        computed:{                 //方法1
+//            sum(){
+//                return this.$store.state.count;
+//            },
+//        },
+//        computed:mapState({                  //方法2
+//            sum1(state){
+//                return state.count;
+//            }
+//        }),
+
+//        computed:mapState(["count"]),         //方法3
     }
 </script>
