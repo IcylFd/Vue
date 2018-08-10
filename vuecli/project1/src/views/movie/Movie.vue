e83ab0438ccdd8bf546d695a8650032d4ef50e20
lifangdi <772167928@qq.com>
2018-08-10 16:32:41 +0800

    2018.8.10 project

--- vuecli/project1/src/views/movie/Movie.vue
+++ vuecli/project1/src/views/movie/Movie.vue
@@ -3,14 +3,38 @@
         <div class="container">
             电影
             <ul>
-                <li v-for="(items,index) in dataList" :key="index+'dl'">{{items.title}}</li>
+                <li v-for="(items,index) in dataList" :key="index+'dl'">
+                    <!--<img class="movieImg" :src="items.images.small" alt="">-->
+                    <img class="movieImg" src="https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2521648155.jpg" alt="">
+                    <div class="movieTitle">{{items.title}}</div>
+                    <div>
+                        <span class="casts" v-for="(item,index) in items.casts" :key="index+'castl'">{{item.name}}/</span>
+                    </div>
+                    <div>
+                        已有
+                        <span class="directors" v-for="(item,index) in items.directors" :key="index+'direl'">{{item.name}}/</span>
+                        观看
+                    </div>
+                    <div>
+                        类型：
+                        <span class="genres" v-for="(item,index) in items.genres" :key="index+'generl'">{{item}}/</span>
+                    </div>
+                </li>
             </ul>
         </div>
     </div>
 </template>
 
 <style scoped>
-
+    .container li{
+        border-bottom: 0.1rem solid #9da0a4;
+    }
+    .movieImg{
+        height:2rem;
+        position:absolute;
+        left:1rem;
+        top:0.3rem;
+    }
 </style>
 
 <script>
