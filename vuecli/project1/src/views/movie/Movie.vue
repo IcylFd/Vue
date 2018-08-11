5927002f5ed263c1179bb19638526c0e7ea0d738
lifangdi <772167928@qq.com>
2018-08-11 16:14:35 +0800

    2018.8.11 project

--- vuecli/project1/src/views/movie/Movie.vue
+++ vuecli/project1/src/views/movie/Movie.vue
@@ -3,8 +3,8 @@
         <div class="container">
             <ul>
                 <li v-for="(items,index) in dataList" :key="index+'dl'">
-                    <img class="movieImg" :src="items.images.small" alt="">
-                    <div class="movieInfo">
+                    <div><img class="movieImg" :src="items.images.small" alt=""></div>
+                    <div class="movieInfo clearfix" >
                         <div class="movieTitle">{{items.title}}</div>
                         <div>
                             <span class="casts" v-for="(item,index) in items.casts" :key="index+'castl'">{{item.name}}/</span>
@@ -21,7 +21,7 @@
 
                 </li>
             </ul>
-            <div class="loading" :class="{toBottom:isBottom}">
+            <div :class="['loading',{toBottom:isFinish}]">
                 <img src="../../assets/img/loading.png" alt="">
             </div>
         </div>
@@ -31,32 +31,51 @@
 <style scoped>
     .container li{
         border-bottom: 0.01rem solid #9da0a4;
+        padding:0.2rem;
+        display: flex;
     }
+    /*.container li>div{*/
+        /*width:0;*/
+    /*}*/
     .movieImg{
+        width:1.5rem;
         height:2rem;
         margin-left:0.3rem;
-        margin-top:0.2rem;
-        /*float:left;*/
+        flex-grow:1;
     }
     .movieInfo{
-        float:left;
+        margin-left:0.3rem;
+        word-break: break-all;
+        flex-grow:2;
+    }
+    .movieTitle{
+        font-size:0.3rem;
+        font-weight:bold;
     }
     .loading{
         height:1.5rem;
         width:1.5rem;
         border-radius: 0.3rem;
         background: rgba(0,0,0,.4);
-        margin:0 auto;
         line-height:100%;
         text-align: center;
         display: none;
+        position: fixed;
+        top:50%;
+        left:50%;
+        margin-left:-0.75rem;
+        margin-top:-0.75rem;
     }
     .loading img{
         height: 0.8rem;
         width: 0.8rem;
     }
     .toBottom{
-        display: block;
+        display: block !important;
+    }
+    .clearfix::after{
+        content:"";
+        clear:both;
     }
 </style>
 
@@ -75,32 +94,40 @@
                     length:10,
                     num:10
                 },
-                isBottom:false
+                isFinish:true
             }
         },
         methods:{
+            //API_PROXY+'https://api.douban.com/v2/movie/in_theaters'
             getData(){
-                axios.get(API_PROXY+'https://api.douban.com/v2/movie/in_theaters')
+                this.isFinish = false;
+                axios.get('https://api.myjson.com/bins/nsb9g')
                     .then((response) => {
-                        console.log(response);
-                        this.dataList = response.data.subjects;
+                        this.dataList = this.dataList.concat(response.data.subjects);
                     })
                     .catch((error) => {
-                        console.log(error);
+
                     });
 //+?start=this.data.length+'&count='+this.data.num
             }
+        },
+        mounted(){
+            window.addEventListener("scroll", ()=>{
+                let scrollTop = document.documentElement.scrollTop;
+                let scrollHeigth = document.documentElement.scrollHeight;
+                let clientHeight = document.documentElement.clientHeight;
+                while(scrollHeigth <= scrollTop + clientHeight + 1){
+                    if(this.isFinish){
+                        this.getData();
+                    }
+
+
+                }
+            });
         }
     }
 
-    window.addEventListener("scroll", function(){
-        let scrollTop = document.documentElement.scrollTop;
-        let scrollHeigth = document.documentElement.scrollHeight;
-        let clientHeight = document.documentElement.clientHeight;
-        if(scrollHeigth <= scrollTop + clientHeight + 10){
 
-        }
-    });
 
 
 </script>
--- vuecli/project1/src/views/music/MusicDetails.vue
+++ vuecli/project1/src/views/music/MusicDetails.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container">
-        <aplayer autoplay showLrc :music="dataList[0]" :list="dataList" :lrc="musicLrc" :slot="musicLrc[0]"/>
+        <aplayer v-if="isLoaded" showLrc :music="dataList[0]" :list="dataList" :lrc="musicLrc" :slot="musicLrc[0]"/>
     </div>
 </template>
-<style>
+<style scoped>
     .container{
         margin-top:1.1rem;
     }
@@ -19,7 +19,8 @@
         data(){
             return{
                 dataList:[],
-                musicLrc:[]
+                musicLrc:[],
+                isLoaded:false
             }
         },
         created(){
@@ -30,78 +31,19 @@
             getData(){
                 axios.get('./data/musicdata.json')
                     .then((res)=>{
-                        console.log(res);
                         this.dataList = res.data.musicData;
+                        this.isLoaded = true;
+                        for(let i = 0; i < res.data.musicData.length; i++){
+                            this.dataList[i].lrc = location.origin + res.data.musicData[i];
+
+                        }
                     })
                     .catch(()=>{
 
                     });
-//                axios.get('./lrc/假如爱有天意.lrc')
-//                    .then((res)=>{
-//                        this.musicLrc4 = res;
-//                    })
-//                    .catch(()=>{
-//
-//                    });
-//                axios.get('./lrc/刚好遇见你-李玉刚.lrc')
-//                    .then((res)=>{
-//                        this.musicLrc6 = res;
-//                    })
-//                    .catch(()=>{
-//
-//                    });
-//                axios.get('./lrc/十年.lrc')
-//                    .then((res)=>{
-//                        this.musicLrc5 = res;
-//                    })
-//                    .catch(()=>{
-//
-//                    });
-//                axios.get('./lrc/就是爱你.lrc')
-//                    .then((res)=>{
-//                        this.musicLrc3 = res;
-//                    })
-//                    .catch(()=>{
-//
-//                    });
-//                axios.get('./lrc/成都-赵雷.lrc')
-//                    .then((res)=>{
-//                        this.musicLrc2 = res;
-//                    })
-//                    .catch(()=>{
-//
-//                    });
-//                axios.get('./lrc/我要你.lrc')
-//                    .then((res)=>{
-//                        this.musicLrc1 = res;
-//                    })
-//                    .catch(()=>{
-//
-//                    });
-                axios.get('./lrc')
-                    .then((res)=>{
-                        getLrc(res);
-                    })
-                    .catch(()=>{
-
-                    });
-
             },
 
         }
     }
-    function getLrc(val){
-        for(let i = 0; i < 6; i++){
-            axios.get('./lrc/'+val.data[i])
-                .then((res)=>{
-//                console.log(res.data);
-                    this.musicLrc = res;
-                    console.log(this.musicLrc);
-                })
-                .catch(()=>{
 
-                });
-        }
-
-    }
 </script>
