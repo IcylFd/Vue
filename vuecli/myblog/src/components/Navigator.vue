3a260ec21f413507a298d6a11129b46bbcfa87fb
lifangdi <772167928@qq.com>
2018-10-09 21:24:31 +0800

    10.9

---
+++ vuecli/myblog/src/components/Navigator.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="container">
+        <div class="start">
+            <div>SENTRA</div>
+        </div>
+        <ul class="nav">
+            <li v-for="(item,index) in navList" :key="index+'nl'" @click="toPage(item.id)">
+                <b :class="{showPoint:item.isSelect}"></b>
+                <span>{{item.pageName}}</span>
+            </li>
+        </ul>
+        <ul class="context">
+            <li>CSDN</li>
+            <li>git</li>
+            <li>微信</li>
+            <li>qq</li>
+            <li>微博</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    template{
+        height:100%;
+    }
+    .container{
+        width:25%;
+        height:100%;
+        background: rgba(0,0,0,.85);
+        position: fixed;
+        top:0;
+        left:0;
+        color:#fff;
+    }
+    .start{
+        height:140px;
+        width:100%;
+        background: #4b3e99;
+        margin:0 auto;
+        line-height: 120px;
+        position: relative;
+    }
+    .start div{
+        height: 80%;
+        width:90%;
+        background: none;
+        border:1px solid rgba(255,255,255,.8);
+        position: absolute;
+        left:50%;
+        top:50%;
+        transform: translateX(-50%) translateY(-50%);
+        text-align: center;
+        font-size: 24px;
+        font-weight: bold;
+    }
+    .nav li b{
+        display: inline-block;
+        height:9px;
+        width:9px;
+        background: none;
+        border:2px solid #fff;
+        border-radius: 50%;
+        line-height: 5px;
+        margin-right: 10px;
+    }
+    .nav li:hover b{
+        background: #fff;
+        transition: all 0.7s ease;
+    }
+    .nav li:hover{
+        font-weight: bold;
+    }
+    .nav li .showPoint{
+        display: inline-block;
+        height:9px;
+        width:9px;
+        background: #fff;
+        border-radius: 50%;
+        line-height: 5px;
+        margin-right: 10px;
+    }
+    .nav li{
+        margin: 10px;
+        transition: all 0.7s ease;
+    }
+    .nav{
+        font-size: 16px;
+        text-align: left;
+        position: absolute;
+        left:20%;
+        top:200px;
+    }
+    .context{
+        position: absolute;
+        bottom:10px;
+        display: flex;
+        left:50%;
+        transform:translateX(-50%);
+    }
+    .context{
+        flex:1;
+    }
+
+</style>
+
+<script>
+    export default {
+        data(){
+            return {
+                navList:[
+                    {
+                        id:'0',
+                        pageName:'Home',
+                        path:'/',
+                        isSelect:true,
+                    },
+                    {
+                        id:'1',
+                        pageName:'Blog',
+                        path:'/blog',
+                        isSelect:false,
+
+                    },
+                    {
+                        id:'2',
+                        pageName:'Resume',
+                        path:'/resume',
+                        isSelect:false,
+
+                    },
+                ],
+            }
+        },
+        methods:{
+            toPage(id){
+                for(let i = 0; i < this.navList.length; i++){
+                    if(this.navList[i].isSelect){
+                        this.navList[i].isSelect = false;
+                    }
+                }
+                this.navList[id].isSelect = true;
+
+                this.$router.push(this.navList[id].path);
+            }
+        }
+    }
+</script>
--- vuecli/myblog/src/views/blog/Blog.vue
+++ vuecli/myblog/src/views/blog/Blog.vue
@@ -13,14 +13,14 @@
         <div class="content">
             <div class="monthBar">
                 <ul>
-                    <li v-for="(items,index) in dataList" :key="index+'dl'">
-                        {{items.date}}
+                    <li v-for="(items,index) in monthlist" :key="index+'dl'" @click='clickMonthBar(items)'>
+                        {{items}}
                     </li>
                 </ul>
             </div>
             <div class="separator"></div>
             <div>
-                <div class="article" v-for="(item,index) in dataList[0].article" :key="index+'al'">
+                <div class="article" v-for="(item,index) in article" :key="index+'al'" v-if="item.flag==true">
                     <img :src="item.img" alt="">
                     <div class="info">
                         <p class="title">{{item.title}}</p>
@@ -82,6 +82,11 @@
         font-weight: bold;
         color:#232323;
         line-height: 80px;
+        transition: all 0.6s ease;
+    }
+    .monthBar ul li:hover{
+        background-color: #4b3e99;
+        color:#f4f4f4;
     }
     .separator{
         width: 100%;
@@ -92,17 +97,50 @@
         display: flex;
         height:300px;
         width:90%;
-        margin: 0 auto;
+        /*margin: 0 auto;*/
         border-bottom:1px solid #ddd;
-        text-align: center;
+        text-align: left;
+        transform: translateY(10%);
     }
     .article img{
         flex:1;
         width:220px;
         height:220px;
+        transform: translateX(30px);
     }
     .article .info{
         flex:2;
+        transform: translateX(40px);
+    }
+    .info button{
+        height:45px;
+        width:160px;
+        background-color: #4b3e99;
+        color:#f4f4f4;
+        font-weight: bold;
+        border:none;
+        transition: all 0.6s ease;
+        transform: translateY(32px);
+    }
+    .info button:hover{
+        background-color: #fff;
+        color:#4b3e99;
+    }
+    .info .title{
+        font-size:22px;
+        font-color:#282828;
+        font-weight: bold;
+
+    }
+    .info .date{
+        font-color:#797979;
+        font-weight: bold;
+        font-size:16px;
+    }
+    .info .intro{
+        font-color:#797979;
+        font-size:14px;
+        transform: translateY(15px);
     }
 </style>
 
@@ -110,118 +148,77 @@
     export default {
         data(){
             return{
-                dataList:[
+                article:[
                     {
-                        dateId:0,
-                        date:'July 2018',
-                        article:[
-                            {
-                                id:'0',
-                                title:'Integer ultrices augue',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_1.jpg',
-                                day:'25 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                            {
-                                id:'1',
-                                title:'Cras commodo odio ut',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_2.jpg',
-                                date:'16 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                            {
-                                id:'2',
-                                title:'Sed at massa turpis',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_3.jpg',
-                                date:'10 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-
-
-                        ]
+                        id:'0',
+                        title:'7Integer ultrices augue',
+                        img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_1.jpg',
+                        month:'July 2018',
+                        day:'25 July 2018',
+                        intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
+                        flag:true,
                     },
                     {
-                        dateId:1,
-                        date:'June 2018',
-                        article:[
-                            {
-                                id:'0',
-                                title:'Integer ultrices augue',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_1.jpg',
-                                day:'25 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                            {
-                                id:'1',
-                                title:'Cras commodo odio ut',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_2.jpg',
-                                day:'16 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                            {
-                                id:'2',
-                                title:'Sed at massa turpis',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_3.jpg',
-                                day:'10 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                        ]
+                        id:'1',
+                        title:'7Cras commodo odio ut',
+                        img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_2.jpg',
+                        month:'July 2018',
+                        day:'16 July 2018',
+                        intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
+                        flag:true,
                     },
                     {
-                        dateId:2,
-                        date:'May 2018',
-                        article:[
-                            {
-                                id:'0',
-                                title:'Integer ultrices augue',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_1.jpg',
-                                day:'25 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                            {
-                                id:'1',
-                                title:'Cras commodo odio ut',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_2.jpg',
-                                day:'16 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                            {
-                                id:'2',
-                                title:'Sed at massa turpis',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_3.jpg',
-                                day:'10 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                        ]
+                        id:'2',
+                        title:'7Sed at massa turpis',
+                        img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_3.jpg',
+                        month:'July 2018',
+                        day:'10 July 2018',
+                        intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
+                        flag:true,
                     },
                     {
-                        dateId:3,
-                        date:'April 2018',
-                        article:[
-                            {
-                                id:'0',
-                                title:'Integer ultrices augue',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_1.jpg',
-                                day:'25 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                            {
-                                id:'1',
-                                title:'Cras commodo odio ut',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_2.jpg',
-                                day:'16 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                            {
-                                id:'2',
-                                title:'Sed at massa turpis',
-                                img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_3.jpg',
-                                day:'10 July 2018',
-                                intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
-                            },
-                        ]
-                    }
+                        id:'3',
+                        title:'6Integer ultrices augue',
+                        img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_2.jpg',
+                        month:'June 2018',
+                        day:'25 June 2018',
+                        intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
+                        flag:false,
+                    },
+                    {
+                        id:'4',
+                        title:'6Cras commodo odio ut',
+                        img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_3.jpg',
+                        month:'June 2018',
+                        day:'16 June 2018',
+                        intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
+                        flag:false,
+                    },
+                    {
+                        id:'5',
+                        title:'6Sed at massa turpis',
+                        img:'http://demo.cssmoban.com/cssthemes5/tpmo_518_sentra/img/blog_1.jpg',
+                        month:'June 2018',
+                        day:'10 June 2018',
+                        intro:'Nam vel egestas nisi. Nullam lobortis magna at enim venenatis luctus. Nam finibus, mauris eu dictum iaculis, dolor tortor cursus quam, in volutpat augue lectus sed magna. Integer mollis lorem quis ipsum maximus finibus.',
+                        flag:false,
+                    },
                 ],
+                monthlist:[
+                    'July 2018','June 2018','May 2018','April 2018'
+                ],
+            }
+        },
+        methods:{
+            clickMonthBar(that){
+                if(this.article.month == that){
+                    this.article.flag = true;
+                    console.log(1);
+                }else{
+                    this.article.flag = false;
+                    console.log(2);
+
+                }
             }
         }
     }
